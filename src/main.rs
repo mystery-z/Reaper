7da12be89f89af7f25ea5c0ed7dfc293b53fc6fd
iCraft85 <aryanb.in@gmail.com>
2023-02-27 09:08:45 +0800

    e

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
 use clap::Parser;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+} 
+
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
