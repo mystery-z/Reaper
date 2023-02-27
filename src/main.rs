8ad7f7bb31100b4b799e8955036863f253b73094
ACE-OF-DIAMONDS <53719487+mystery-z@users.noreply.github.com>
2023-02-27 23:13:12 +0800

    OMG this took too long

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,19 @@ use std::fs::OpenOptions;
 //right now, the problem with the logging function is that it just gets the file name, but it should get the path of the file as well, this can be fixed later when we add the actual del func
 fn logging(){ 
     let args = Args::parse();   
-    println!("istg ")
-    println!("{:?}", args); //for debugging only
+    
+    //println!("{:?}", args); //for debugging only
     if !Path::new("reaper_log.log").exists(){
         let log_file = File::create("reaper_log.log"); 
     }
  
-    let log_file= File::open("reaper_log.log");
-    let mut log_file = OpenOptions::new().write(true).create_new(true).open("reaper_log.log").expect("Unable to open/create file"); //create the reaper_log.log file
+    //let log_file= File::open("reaper_log.log");
+    let mut log_file = OpenOptions::new().write(false).append(true).create(false).open("reaper_log.log").expect("Unable to open/create file"); //create the reaper_log.log file
     let result_to_log = args.files[0].as_path().display().to_string();
     let newline = "";
     let result = [result_to_log, newline.to_string()].join("\n");
-    println!("{:}", result);
-    gets the file name, then file_name --> path --> string --> bytes and writes to .log
+    //println!("{:}", result); //for debugging only
+    //gets the file name, then file_name --> path --> string --> bytes and writes to .log
     log_file.write_all(result.as_bytes()).expect("Unable to write data");
 }
 
@@ -47,11 +47,12 @@ fn main() -> Result<(), anyhow::Error> {
     }
 
     for file in &args.files {
+        logging();
         if !Path::new(&file).exists() {
             println!("reap: Cannot remove {:?}: no such file or directory", file);
             continue;
         }
-        logging();
+       
         let absolute_path = fs::canonicalize(file).unwrap();
 
         let mut grave = PathBuf::from("/tmp/grave");
