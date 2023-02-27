81acb29e856ac3a4959cc8af8c3f886fb931094b
ACE-OF-DIAMONDS <53719487+mystery-z@users.noreply.github.com>
2023-02-27 22:28:00 +0800

    asdfasdf

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,31 @@ use std::io::Error;
 use std::path::{Path, PathBuf};
 
 use args::Args;
+use std::fs::File;
+use std::io::Write;
+use std::fs::OpenOptions;
+
+
+//right now, the problem with the logging function is that it just gets the file name, but it should get the path of the file as well, this can be fixed later when we add the actual del func
+fn logging(){ 
+    let args = Args::parse();   
+    println!("istg ")
+    println!("{:?}", args); //for debugging only
+    if !Path::new("reaper_log.log").exists(){
+        let log_file = File::create("reaper_log.log"); 
+    }
+ 
+    let log_file= File::open("reaper_log.log");
+    let mut log_file = OpenOptions::new().write(true).create_new(true).open("reaper_log.log").expect("Unable to open/create file"); //create the reaper_log.log file
+    let result_to_log = args.files[0].as_path().display().to_string();
+    let newline = "";
+    let result = [result_to_log, newline.to_string()].join("\n");
+    println!("{:}", result);
+    gets the file name, then file_name --> path --> string --> bytes and writes to .log
+    log_file.write_all(result.as_bytes()).expect("Unable to write data");
+}
+
+
 
 fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
@@ -26,7 +51,7 @@ fn main() -> Result<(), anyhow::Error> {
             println!("reap: Cannot remove {:?}: no such file or directory", file);
             continue;
         }
-
+        logging();
         let absolute_path = fs::canonicalize(file).unwrap();
 
         let mut grave = PathBuf::from("/tmp/grave");
